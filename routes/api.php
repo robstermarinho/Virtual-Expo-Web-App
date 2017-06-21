ae18b0499acdab0825765481d940885e98fd54d4
˜Robert <robstermarinho@gmail.com>
2017-06-21 15:13:08 -0300

    Authentication
    Creating the Authentication service.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+ * Events
+ */
+Route::resource('events', 'Event\EventController', ['only' => ['index', 'show']]);
